40dad9251cd2e5f39e76c84b1208aa901cb2b8a9
wh_glb <wh22wanhan@126.com>
2021-12-21 14:53:51 +0800

    增加 springbootTest 部分

--- automateInterface/src/main/java/com/shenzhou/vo/ReceiveCartonVo.java
+++ automateInterface/src/main/java/com/shenzhou/vo/ReceiveCartonVo.java
@@ -6,8 +6,10 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
-@ApiModel("中转仓接收包装箱成功！")
+@ApiModel("ReceiveCartonVo:中转仓接收包装箱vo")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
